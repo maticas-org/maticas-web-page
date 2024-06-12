4454613c0455c9991c6dd819eb8eaa9f4d200a52
DaveAlsina <davealsina@gmail.com>
2024-06-12 11:14:35 -0500

    Update App.tsx
    added redirection from '/' to '/es'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 import  Home  from './pages/Home';
 import DataLoggerLanding from './pages/DataLoggerLanding';
@@ -9,12 +9,21 @@ import VPDArticle from './pages/blog/vpd/VPD';
 import { ContactUs } from './pages/ContactUs';
 import { StillWorkingOnIt } from './pages/StillWorkingOnIt';
 
+const RedirectToEs = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/es', { replace: true });
+  }, [navigate]);
+  return null;
+};
+
 function App() {
   return (
     <>
     <Suspense fallback={<div>Cargando...</div>}>
     <Router>
         <Routes>
+          <Route path="/" element={<RedirectToEs />} />
           <Route path="/es" element={<Home />} />
           <Route path="/es/blog" element={<Blog/>} />
           <Route path="/es/blog/vpd" element={<VPDArticle/>} />
