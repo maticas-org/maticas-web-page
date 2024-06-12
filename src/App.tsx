ae7b033c067b200cf0dd2e072830d2f512492eb3
DaveAlsina <davealsina@gmail.com>
2024-06-11 21:39:04 -0500

    Update App.tsx
    Add loading suspence callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import  Home  from './pages/Home';
 import DataLoggerLanding from './pages/DataLoggerLanding';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Blog } from './pages/blog/Blog';
 import VPDArticle from './pages/blog/vpd/VPD';
 import { ContactUs } from './pages/ContactUs';
@@ -11,8 +12,8 @@ import { StillWorkingOnIt } from './pages/StillWorkingOnIt';
 function App() {
   return (
     <>
+    <Suspense fallback={<div>Cargando...</div>}>
     <Router>
-      <>
         <Routes>
           <Route path="/es" element={<Home />} />
           <Route path="/es/blog" element={<Blog/>} />
@@ -21,8 +22,8 @@ function App() {
           <Route path="/es/servicios/registrador-de-datos" element={<DataLoggerLanding />} />
           <Route path="*" element={<StillWorkingOnIt />} />
         </Routes>
-      </>
     </Router>
+    </Suspense>
     </>
   )
 }
