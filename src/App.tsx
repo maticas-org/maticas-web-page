4b4c84d641322ff15166e66503ec152f91893d11
DaveAlsina <david.alsina@loka.com>
2025-06-07 18:46:17 -0500

    update, added helmet to app file

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import VPDArticle from './pages/blog/vpd/VPD';
 import DLIArticle from './pages/blog/dli/DLI';
 import { ContactUs } from './pages/ContactUs';
 import { StillWorkingOnIt } from './pages/StillWorkingOnIt';
+import { Helmet } from 'react-helmet';
 
 const RedirectToEs = () => {
   const navigate = useNavigate();
@@ -21,6 +22,53 @@ const RedirectToEs = () => {
 function App() {
   return (
     <>
+    <Helmet>
+      <title>Inicio | Maticas Tech - Soluciones Agrícolas Inteligentes</title>
+      <meta name="description" content="Bienvenido a Maticas Tech, su proveedor principal de soluciones agrícolas inteligentes utilizando IA, IoT y aprendizaje automático para una agricultura eficiente y gestión de datos." />
+      <meta name="keywords" content="agricultura inteligente, IA en agricultura, aprendizaje automático en agricultura, registrador de datos, visualización de datos, análisis de datos, IoT en agricultura, Maticas Tech" />
+
+      <meta property='og:title' content='Inicio | Maticas Tech - Soluciones Agrícolas Inteligentes'/>
+      <meta property='og:description' content='Bienvenido a Maticas Tech, su proveedor principal de soluciones agrícolas inteligentes utilizando IA, IoT y aprendizaje automático para una agricultura eficiente y gestión de datos.'/>
+      <meta property="og:image" content="https://www.maticas-tech.com/maticas-tech-logo-color-2.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      <script type="application/ld+json">
+        {JSON.stringify({
+          "@context": "http://schema.org",
+          "@type": "WebPage",
+          "name": "Inicio",
+          "description": "Bienvenido a Maticas Tech, su proveedor principal de soluciones de agricultura inteligente utilizando IA, IoT y aprendizaje automático para una gestión eficiente de la agricultura y los datos.",
+          "url": "http://www.maticas-tech.com/es",
+          "inLanguage": "es",
+          "breadcrumb": {
+            "@type": "BreadcrumbList",
+            "itemListElement": [
+              {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Inicio",
+                "item": "http://www.maticas-tech.com/es"
+              }
+            ]
+          },
+          "author": {
+            "@type": "Organization",
+            "name": "Maticas Tech",
+            "url": "http://www.maticas-tech.com/es"
+          },
+          "publisher": {
+            "@type": "Organization",
+            "name": "Maticas Tech",
+            "url": "http://www.maticas-tech.com/es"
+          },
+          "datePublished": "2024-06-11",
+          "dateModified": "2024-06-11"
+        })}
+      </script>
+    </Helmet>
+
     <Suspense fallback={<div>Cargando...</div>}>
     <Router>
         <Routes>
